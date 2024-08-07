84ef0bbcd1e349eb71561d898ed3f61dbc9cd3ce
Hayeddine <hay.eddine8@gmail.com>
2024-08-07 18:23:53 +0100

    fixed: dynamic footer date, all projects link, js type errror

--- js/main.js
+++ js/main.js
@@ -6,11 +6,11 @@ const iconContainer = document.querySelector('.dark-mode');
 let dark = true;
 
 iconContainer.addEventListener('click', () => {
-    const theme = dark ? 'light' : 'dark';
-    const icon = dark ? darkIcon : lightIcon;
-    document.documentElement.setAttribute('theme', theme);
-    iconContainer.innerHTML = icon;
-    dark = !dark;
+	const theme = dark ? 'light' : 'dark';
+	const icon = dark ? darkIcon : lightIcon;
+	document.documentElement.setAttribute('theme', theme);
+	iconContainer.innerHTML = icon;
+	dark = !dark;
 })
 
 // Menu =======================================================================================
@@ -22,29 +22,29 @@ const overlay = document.querySelector('.overlay');
 let menuShow = false;
 
 menuLinks.forEach(link => {
-    link.onclick = () => {
-        if (menuShow === true) {
-            menuIcon.click();
-        }
-        menuLinks.forEach(link => link.classList.remove('active'));
-        link.classList.add('active');
-    }
+	link.onclick = () => {
+		if (menuShow === true) {
+			menuIcon.click();
+		}
+		menuLinks.forEach(link => link.classList.remove('active'));
+		link.classList.add('active');
+	}
 });
 
 overlay.onclick = () => menuIcon.click();
 
-menuIcon.addEventListener('click', function () {
-    if (menuShow === false) {
-        menuIcon.classList.add('animate-menu');
-        theMenu.classList.add('show');
-        overlay.style.display = 'block';
-        menuShow = true;
-    } else {
-        menuIcon.classList.remove('animate-menu');
-        theMenu.classList.remove('show');
-        overlay.style.display = 'none';
-        menuShow = false;
-    }
+menuIcon.addEventListener('click', function() {
+	if (menuShow === false) {
+		menuIcon.classList.add('animate-menu');
+		theMenu.classList.add('show');
+		overlay.style.display = 'block';
+		menuShow = true;
+	} else {
+		menuIcon.classList.remove('animate-menu');
+		theMenu.classList.remove('show');
+		overlay.style.display = 'none';
+		menuShow = false;
+	}
 });
 
 // Scroll display =======================================================================================
@@ -53,41 +53,41 @@ const scrollToSection = document.querySelectorAll('.scroll');
 const landing = document.querySelector('.landing');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 1) {
-        document.querySelector('header').classList.add('fix-header');
-    } else {
-        document.querySelector('header').classList.remove('fix-header');
+	if (window.scrollY > 1) {
+		document.querySelector('header').classList.add('fix-header');
+	} else {
+		document.querySelector('header').classList.remove('fix-header');
 
-    }
-    if (window.scrollY > 120) {
-        scrollToSection[0].style.display = 'none';
-    } else {
-        scrollToSection[0].style.display = 'flex';
-    }
-    if (window.scrollY > landing.offsetHeight + 120) {
-        scrollToSection[1].style.display = 'none';
-    } else {
-        scrollToSection[1].style.display = 'flex';
-    }
+	}
+	if (window.scrollY > 120) {
+		scrollToSection[0].style.display = 'none';
+	} else {
+		scrollToSection[0].style.display = 'flex';
+	}
+	if (window.scrollY > landing.offsetHeight + 120) {
+		scrollToSection[1].style.display = 'none';
+	} else {
+		scrollToSection[1].style.display = 'flex';
+	}
 });
 
 scrollToSection.forEach(item => {
-    item.addEventListener('click', () => {
-        menuLinks.forEach(link => {
-            if (item.href === link.firstElementChild.href) {
-                link.click();
-            }
-        })
-    })
+	item.addEventListener('click', () => {
+		menuLinks.forEach(link => {
+			if (item.href === link.firstElementChild.href) {
+				link.click();
+			}
+		})
+	})
 });
 
 let hk = document.querySelector('footer a.developer');
-hk.addEventListener('click', function () {
-    menuLinks.forEach(link => {
-        if (hk.href === link.firstElementChild.href) {
-            link.click();
-        }
-    })
+hk.addEventListener('click', function() {
+	menuLinks.forEach(link => {
+		if (hk.href === link.firstElementChild.href) {
+			link.click();
+		}
+	})
 })
 
 // generalize this for all the links by one logic
@@ -100,32 +100,21 @@ const skillsCont = document.querySelectorAll('#skills .skills');
 const skills = document.querySelectorAll('#skills .skills .skill');
 
 skillFilter.forEach(item => {
-    item.addEventListener('click', function () {
-        skillFilter.forEach(item => {
-            item.classList.remove('active')
-        })
-        skillsCont.forEach(cont => {
-            cont.style.display = 'none';
-            cont.classList.remove('animate-skills')
-        })
-        this.classList.add('active')
+	item.addEventListener('click', function() {
+		skillFilter.forEach(item => {
+			item.classList.remove('active')
+		})
+		skillsCont.forEach(cont => {
+			cont.style.display = 'none';
+			cont.classList.remove('animate-skills')
+		})
+		this.classList.add('active')
 
-        let target = document.querySelector(this.dataset.skill);
-        let skillsOfTarget = document.querySelectorAll(`${this.dataset.skill} .skill`);
-        target.style.display = 'flex';
-        target.classList.add('animate-skills')
+		let target = document.querySelector(this.dataset.skill);
+		target.style.display = 'flex';
+		target.classList.add('animate-skills')
 
-        skillsOfTarget.forEach(skill => {
-            skill.lastElementChild.firstElementChild.style.width = '0'
-        })
-
-        let animateTarget = setTimeout(function () {
-            skillsOfTarget.forEach(skill => {
-                skill.lastElementChild.firstElementChild.style.width = skill.lastElementChild.firstElementChild.dataset.prog
-
-            })
-        })
-    })
+	})
 });
 
 skillFilter[0].click();
@@ -135,3 +124,7 @@ skillFilter[0].click();
 document.querySelectorAll('#about .social-media .icons a').forEach(link => link.target = '_blank');
 // document.querySelector('#about a.resume').target = '_blank'
 document.querySelectorAll('#projects a').forEach(link => link.target = '_blank');
+
+// Links Target =========================================================================================
+
+document.querySelector("footer span#date").innerHTML = new Date().getFullYear();
