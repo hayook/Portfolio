0a79e6b1aa0fd96067ffcb3177f55ccf9441988c
hayook <hay.eddine8@gmail.com>
2026-03-28 07:31:30 +0100

    removed: header & skills section

--- js/main.js
+++ js/main.js
@@ -1,73 +1,13 @@
-// dark mode ============================================================================================
-
-const darkIcon = '<i class="far fa-moon"></i>';
-const lightIcon = '<i class="far fa-sun"></i>';
-const iconContainer = document.querySelector('.dark-mode');
-let dark = true;
-
-iconContainer.addEventListener('click', () => {
-	const theme = dark ? 'light' : 'dark';
-	const icon = dark ? darkIcon : lightIcon;
-	document.documentElement.setAttribute('theme', theme);
-	iconContainer.innerHTML = icon;
-	dark = !dark;
-})
-
-// Menu =======================================================================================
-
-const menuIcon = document.querySelector('header nav .menu');
-const theMenu = document.querySelector('header nav ul');
-const menuLinks = document.querySelectorAll('header nav ul li');
-const overlay = document.querySelector('.overlay');
-let menuShow = false;
-
-menuLinks.forEach(link => {
-	link.onclick = () => {
-		if (menuShow === true) {
-			menuIcon.click();
-		}
-		menuLinks.forEach(link => link.classList.remove('active'));
-		link.classList.add('active');
-	}
-});
-
-overlay.onclick = () => menuIcon.click();
-
-menuIcon.addEventListener('click', function() {
-	if (menuShow === false) {
-		menuIcon.classList.add('animate-menu');
-		theMenu.classList.add('show');
-		overlay.style.display = 'block';
-		menuShow = true;
-	} else {
-		menuIcon.classList.remove('animate-menu');
-		theMenu.classList.remove('show');
-		overlay.style.display = 'none';
-		menuShow = false;
-	}
-});
-
 // Scroll display =======================================================================================
 
 const scrollToSection = document.querySelectorAll('.scroll');
 const landing = document.querySelector('.landing');
 
 window.addEventListener('scroll', () => {
-	if (window.scrollY > 1) {
-		document.querySelector('header').classList.add('fix-header');
-	} else {
-		document.querySelector('header').classList.remove('fix-header');
-
-	}
 	if (window.scrollY > 120) {
 		scrollToSection[0].style.display = 'none';
 	} else {
 		scrollToSection[0].style.display = 'flex';
-	}
-	if (window.scrollY > landing.offsetHeight + 120) {
-		scrollToSection[1].style.display = 'none';
-	} else {
-		scrollToSection[1].style.display = 'flex';
 	}
 });
 
@@ -92,32 +32,6 @@ hk.addEventListener('click', function() {
 
 // generalize this for all the links by one logic
 // add this property when scrolling
-
-// Skill Seciton ========================================================================================
-
-const skillFilter = document.querySelectorAll('#skills ul li');
-const skillsCont = document.querySelectorAll('#skills .skills');
-const skills = document.querySelectorAll('#skills .skills .skill');
-
-skillFilter.forEach(item => {
-	item.addEventListener('click', function() {
-		skillFilter.forEach(item => {
-			item.classList.remove('active')
-		})
-		skillsCont.forEach(cont => {
-			cont.style.display = 'none';
-			cont.classList.remove('animate-skills')
-		})
-		this.classList.add('active')
-
-		let target = document.querySelector(this.dataset.skill);
-		target.style.display = 'flex';
-		target.classList.add('animate-skills')
-
-	})
-});
-
-skillFilter[0].click();
 
 // Links Target =========================================================================================
 
